a136da57488dc9c4dc4ded6b62dcf4aace9be75e
Yicheng <h0928588211@gmail.com>
2022-05-09 00:50:49 +0800

    test

---
+++ Test/test.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'book',
+  password : 'book',
+  database : 'book'
+});
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));/* 將預設路徑設在public*/
+
+
+app.listen(8080);
+
+// connection.query("SELECT * FROM product", function(err, result, fields){ //查詢users的所有資料    
+//   if(err) throw err;
+//     console.log(result);
+//   });
+//   console.log( 'select ended!' );
