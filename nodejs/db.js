a59b05dff9db008fb50874fb9693f0e685384649
Yicheng <h0928588211@gmail.com>
2022-05-08 17:43:08 +0800

    loginPractice

--- nodejs/db.js
+++ nodejs/db.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const mysql      = require('mysql');
+const mysql   = require('mysql');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -22,4 +22,17 @@ const app = express();
 app.use(express.static(__dirname + '/public'));/* 將預設路徑設在public*/
 
 
-app.listen(8080);
+app.listen(8081);
+
+
+
+// connection.query("SELECT * FROM product ", function(err, result, fields){   
+//   if(err) throw err;      //查詢type 為 learn 的資料
+//     //console.log(result);
+//     var filterbook = result.filter(function(item,idex,array){
+//       return item.book_type=='learn'
+//     });
+//     console.log(filterbook) //filterbook 為儲存type 為 learn 的書籍 陣列
+//   });
+//   console.log( 'select ended!' );
+
