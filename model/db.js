0730b671546eecd88279216ae789a901d8828728
Liuming <a723479.a12@gmail.com>
2022-05-09 22:31:17 +0800

    modify

--- model/db.js
+++ model/db.js
@@ -25,13 +25,13 @@ app.listen(8081);
 
 
 
-// connection.query("SELECT * FROM product ", function(err, result, fields){   
-//   if(err) throw err;      //查詢type 為 learn 的資料
-//     //console.log(result);
-//     var filterbook = result.filter(function(item,idex,array){
-//       return item.book_type=='learn'
-//     });
-//     console.log(filterbook) //filterbook 為儲存type 為 learn 的書籍 陣列
-//   });
-//   console.log( 'select ended!' );
+connection.query("SELECT * FROM product ", function(err, result, fields){   
+  if(err) throw err;      //查詢type 為 learn 的資料
+    //console.log(result);
+    var filterbook = result.filter(function(item,idex,array){
+      return item.book_type=='learn'
+    });
+    console.log(filterbook) //filterbook 為儲存type 為 learn 的書籍 陣列
+  });
+  console.log( 'select ended!' );
 
