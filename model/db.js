6ec130ab0efabd866b75668253ac68713d98280b
Liuming <a723479.a12@gmail.com>
2022-05-10 17:09:21 +0800

    server

--- model/db.js
+++ model/db.js
@@ -1,6 +1,9 @@
 const express   = require('express');
-const mysql   = require('mysql');
+const mysql     = require('mysql');
+const { json }  = require('express/lib/response');
 
+
+//start to connect db
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'book',
@@ -16,14 +19,9 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
 });
 
+
 const app = express();
-
-app.use(express.static(__dirname + '/public'));/* 將預設路徑設在public*/
-
-
-app.listen(8081);
-
-
+  
 
 connection.query("SELECT * FROM product ", function(err, result, fields){   
   if(err) throw err;      //查詢type 為 learn 的資料
@@ -32,6 +30,15 @@ connection.query("SELECT * FROM product ", function(err, result, fields){
       return item.book_type=='learn'
     });
     console.log(filterbook) //filterbook 為儲存type 為 learn 的書籍 陣列
-  });
-  console.log( 'select ended!' );
+    console.log( 'select ended!' );
+});
 
+
+app.get('/',function(req,res){
+  console.log('Good night Taichung');
+  res.send('Good night Taichung');
+})
+
+app.listen(8080,function(){
+  
+});
---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path    = require('path');
+var app = express();
+
+//建立 server
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/home/home.html');
+})
+app.get('/cart/*',function(req,res){
+  res.sendFile(__dirname + '/cart/cart.html');
+})
+app.get('/chbook/*',function(req,res){
+  res.sendFile(__dirname + '/chbook/chbook.html');
+})
+app.get('/collect/*',function(req,res){
+  res.sendFile(__dirname + '/collect/collect.html');
+})
+app.get('/home/*',function(req,res){
+  res.sendFile(__dirname + '/home/home.html');
+})
+app.get('/login/*',function(req,res){
+  res.sendFile(__dirname + '/login/login.html');
+})
+app.get('/member/*',function(req,res){
+  res.sendFile(__dirname + '/member/member.html');
+})
+app.get('/merchant/*',function(req,res){
+  res.sendFile(__dirname + '/merchant/merchant.html');
+})
+app.get('/order/*',function(req,res){
+  res.sendFile(__dirname + '/order/order.html');
+})
+app.get('/product/*',function(req,res){
+  res.sendFile(__dirname + '/product/index.html');
+})
+app.get('/rebook/*',function(req,res){
+  res.sendFile(__dirname + '/rebook/rebook.html');
+})
+app.get('/register/*',function(req,res){
+  res.sendFile(__dirname + '/register/register.html');
+})
+app.get('/service/*',function(req,res){
+  res.sendFile(__dirname + '/service/service.html');
+})
+
+
+app.use(express.static('static')); //讀取靜態檔案
+app.use('*/images',express.static(path.join(__dirname, 'static/images')));
+app.use('*/js'    ,express.static(path.join(__dirname, 'static/js')));
+app.use('*/css'   ,express.static(path.join(__dirname, 'static/css')));
+
+var server = app.listen(8000,function(){ //8000這個port
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("listen on http://%s:%s", host, port)
+})
+
+
