ac1129b42f6a0ea741e0e1c9ac48d8159787bb2e
Liuming <a723479.a12@gmail.com>
2022-05-20 19:08:04 +0800

    router

--- route/modules/cart.js
+++ route/modules/cart.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const cartController = require('../../controllers/cartcontroller')
 
 
-
+router.get('/', cartController.cartPage)
 router.get('/cart', cartController.cartPage)
 
 
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var path    = require('path');
 var db      = require('./route/modules/db');
 var app     = express();
 
-
+app.set('views', './views')
 app.set('view engine', 'ejs');
 
 
 //建立 server
-<<<<<<< HEAD
+
 db.conn();
 
 
@@ -58,79 +58,77 @@ db.conn();
 // })
 
 // db.conn();
-app.get('/',function(req,res){
-  res.render('home',{
-    'howmanybook':'1',
-    'book.name':'書本1',
-    'book.no':'00000000',
-    'book.writer':'劉銘',
-    'book.money':'666'
-  });
-  // db.select('learn');
-})
-app.get('/cart/*',function(req,res){
-  res.render('cart',{
-    'howmanybook':'1',
-    'book.name':'書本1',
-    'book.no':'00000000',
-    'book.writer':'劉銘',
-    'book.money':'666',
-    'total':'666'
-  });
-})
-app.get('/chbook/*',function(req,res){
-  res.render('chbook');
-})
-app.get('/classification/*',function(req,res){
-  res.render('classification');
-})
-app.get('/collect/*',function(req,res){
-  res.render('collect',{
-    'howmanybook':'1',
-    'book.name':'書本1',
-    'book.no':'00000000',
-    'book.writer':'劉銘',
-    'book.money':'666'
-  });
-})
-app.get('/home/*',function(req,res){
-  res.render('home');
-})
-app.get('/login/*',function(req,res){
-  res.render('login');
-})
-app.get('/member/*',function(req,res){
-  res.render('member',{
-    'name':'劉銘',
-    'email':'liuliuming@liumail.com',
-    'phone':'0986138613',
-    'password':'098613'
-  });
-})
-app.get('/merchant/*',function(req,res){
-  res.render('merchant');
-})
-app.get('/order/*',function(req,res){
-  res.render('order');
-})
-app.get('/product/*', function (req, res) {
-  res.render('product',{
-    'bookname':'書本1',
-    'book.no':'00000000',
-    'book.writer':'劉銘',
-    'book.money':'666'
-  });
-})
-app.get('/rebook/*',function(req,res){
-  res.render('rebook');
-})
-app.get('/register/*',function(req,res){
-  res.render('register');
-})
-app.get('/service/*',function(req,res){
-  res.render('service');
-})
->>>>>>> c787e774e8311511aa90ee0b17a475e66ee69475
+// app.get('/',function(req,res){
+//   res.render('home',{
+//     'howmanybook':'1',
+//     'book.name':'書本1',
+//     'book.no':'00000000',
+//     'book.writer':'劉銘',
+//     'book.money':'666'
+//   });
+// })
+// app.get('/cart/*',function(req,res){
+//   res.render('cart',{
+//     'howmanybook':'1',
+//     'book.name':'書本1',
+//     'book.no':'00000000',
+//     'book.writer':'劉銘',
+//     'book.money':'666',
+//     'total':'666'
+//   });
+// })
+// app.get('/chbook/*',function(req,res){
+//   res.render('chbook');
+// })
+// app.get('/classification/*',function(req,res){
+//   res.render('classification');
+// })
+// app.get('/collect/*',function(req,res){
+//   res.render('collect',{
+//     'howmanybook':'1',
+//     'book.name':'書本1',
+//     'book.no':'00000000',
+//     'book.writer':'劉銘',
+//     'book.money':'666'
+//   });
+// })
+// app.get('/home/*',function(req,res){
+//   res.render('home');
+// })
+// app.get('/login/*',function(req,res){
+//   res.render('login');
+// })
+// app.get('/member/*',function(req,res){
+//   res.render('member',{
+//     'name':'劉銘',
+//     'email':'liuliuming@liumail.com',
+//     'phone':'0986138613',
+//     'password':'098613'
+//   });
+// })
+// app.get('/merchant/*',function(req,res){
+//   res.render('merchant');
+// })
+// app.get('/order/*',function(req,res){
+//   res.render('order');
+// })
+// app.get('/product/*', function (req, res) {
+//   res.render('product',{
+//     'bookname':'書本1',
+//     'book.no':'00000000',
+//     'book.writer':'劉銘',
+//     'book.money':'666'
+//   });
+// })
+// app.get('/rebook/*',function(req,res){
+//   res.render('rebook');
+// })
+// app.get('/register/*',function(req,res){
+//   res.render('register');
+// })
+// app.get('/service/*',function(req,res){
+//   res.render('service');
+// })
 
 app.use(express.static('static')); //讀取靜態檔案
 app.use('*/images'    ,express.static(path.join(__dirname, 'static/images')));
