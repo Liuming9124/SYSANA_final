eb7281c22b3612d1721b41928c5813c6b26c6846
Liuming <a723479.a12@gmail.com>
2022-05-22 01:48:55 +0800

    homerender

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,17 +1,43 @@
+var db = require('../route/modules/db')
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'book',
+    password: 'book',
+    database: 'book'
+})
 
 const homeController = {
+
     homePage: (req, res) => {
-        var data = [
-            { id: 1, name: "bob" },
-            { id: 2, name: "john" },
-            { id: 3, name: "jake" },
-        ];
-        
-        JSON.stringify(data)
-        return res.render('home')
+
+        // console.log( db.select('learn'))
+        // var data = [
+        //     { id: 1, name: "bob" },
+        //     { id: 2, name: "john" },
+        //     { id: 3, name: "jake" },
+        // ];
+
+        // JSON.stringify(data)
+        // return res.render('home')
+        connection.query("SELECT * FROM product order By Rand()", function (err, random, fields) {
+            if (err) {
+                throw err;
+            }
+            else {
+                // console.log(random);
+                console.log(random[0]);
+                res.render('home',{
+                    'book_name' : random[0].book_name,
+                    'book_id'   : random[0].book_id,
+                    'book_type' : random[0].book_type,
+                    'book_price': random[0].book_price
+                });
+            }
+        });
     }
-    
+
 }
 
 module.exports = homeController
