a00149e1768623ba5122efda9f9675f85e9ad271
Liuming <a723479.a12@gmail.com>
2022-05-21 20:31:11 +0800

    coinmodify

--- controllers/membercontroller.js
+++ controllers/membercontroller.js
@@ -2,12 +2,13 @@
 
 const memberController = {
     memberPage: (req, res) => {
-        return res.render('member',{
-            'name':'劉銘',
-            'email':'liuliuming@liumail.com',
-            'phone':'0986138613',
-            'password':'098613'
-          })
+        return res.render('member', {
+            'name': '劉銘',
+            'email': 'liuliuming@liumail.com',
+            'phone': '0986138613',
+            'password': '',
+            'bookcoin':'0'
+        })
     }
 }
 
