3d990dc6f52ee94b3998f04e98792f558012456d
Yicheng <h0928588211@gmail.com>
2022-05-22 20:30:44 +0800

    login

--- controllers/logincontroller.js
+++ controllers/logincontroller.js
@@ -1,4 +1,12 @@
+var db = require('../route/modules/db')
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'book',
+    password: 'book',
+    database: 'book'
+})
 
 const loginController = {
     loginPage: (req, res) => {
@@ -6,7 +14,25 @@ const loginController = {
     },
     userlogin: (req, res) => {
         var user = req.body;
-        console.log(user);
+        var compare = "SELECT * FROM users";
+        connection.query(compare, function(err,result){
+            var count=0;
+            // console.log(result);
+            for(var i =0;i<result.length;i++){
+                if(user.username == result[i].email && user.password == result[i].userpassword){
+                    // return res.render('login', { 'result': '帳號或密碼錯誤' });
+                    console.log("登入成功");
+                    break;
+                }
+                else{
+                    count++
+                }
+            }
+            if(count==result.length){
+                console.log("登入失敗");
+            }
+        })
+        // console.log(user.username);
     }
 }
 
