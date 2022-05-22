3f2b2ae7a804bc434b26cb97fcb842c562a9f7ee
Yicheng <h0928588211@gmail.com>
2022-05-22 20:39:16 +0800

    login

--- controllers/logincontroller.js
+++ controllers/logincontroller.js
@@ -13,6 +13,29 @@ const loginController = {
         return res.render('login')
     },
     userlogin: (req, res) => {
+        var user = req.body;
+        var compare = "SELECT * FROM users";
+        connection.query(compare, function(err,result){
+            var count=0;
+            // console.log(result);
+            for(var i =0;i<result.length;i++){
+                if(user.username == result[i].email && user.password == result[i].userpassword){
+                    // return res.render('login', { 'result': '帳號或密碼錯誤' });
+                    console.log("登入成功");
+                    break;
+                }
+                else{
+                    count++
+                }
+            }
+            if(count==result.length){
+                console.log("登入失敗");
+            }
+        })
+        // console.log(user.username);
+
+
+
         // let data = req.body;
         // if (data.username == '') {
         //     return res.send({ resultCode: 400, resultMsg: '使用者名稱為空' });
