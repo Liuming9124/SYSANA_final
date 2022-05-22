16f41661e0d4a0853f71668db6acff835a9d169b
Liuming <a723479.a12@gmail.com>
2022-05-22 17:42:39 +0800

    modify

--- controllers/logincontroller.js
+++ controllers/logincontroller.js
@@ -5,8 +5,25 @@ const loginController = {
         return res.render('login')
     },
     userlogin: (req, res) => {
-        var user = req.body;
-        console.log(user);
+        let data = req.body;
+        if (data.username == '') {
+            return res.send({ resultCode: 400, resultMsg: '使用者名稱為空' });
+        } else if (data.password == '') {
+            return res.send({ resultCode: 400, resultMsg: '密碼為空' });
+        } else {
+            User.findOne({ name: data.name }).then(user => {
+                if (!user) {
+                    return res.send({ resultCode: 400, resultMsg: '使用者不存在' });
+                }
+                if (data.password != user.password) {
+                    return res.send({ resultCode: 400, resultMsg: '密碼錯誤' });
+                }
+                //生成cookie
+                auth.gen_session(user, res);
+                return res.send({ resultCode: 200, resultMsg: '登入成功' });
+            })
+        }
+
     }
 }
 
--- controllers/registercontroller.js
+++ controllers/registercontroller.js
@@ -1,5 +1,7 @@
-var db = require('../route/modules/db')
 const mysql = require('mysql');
+
+// const bcrypt = require('bcrypt');   // 引入 bcrypt
+// const saltRounds = 10;              // 加鹽，增加密碼的複雜程度
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -15,9 +17,9 @@ const registerController = {
     },
     registerAdd: (req, res) => {
         var data = req.body;
-        console.log(data.email);
-        var sql = "INSERT INTO users(email,username,userpassword,phone,useraddr,point) VALUES('"+ data.email +"','"+ data.name +"','" + data.password + "','" + data.phone + " ',' " + data.address+ "',0)";
-        console.log(sql);
+        // data.password = bcrypt.hashSync(data.password, saltRounds);
+        // console.log(data.password);
+        var sql = "INSERT INTO users(email,username,userpassword,phone,useraddr,point) VALUES('" + data.email + "','" + data.name + "','" + data.password + "','" + data.phone + " ',' " + data.address + "',0)";
         connection.query(sql, function (err, result) {
             if (err) {
                 return res.render('register', { 'result': '註冊失敗，此電子信箱已被註冊' });
@@ -26,7 +28,7 @@ const registerController = {
                 return res.render('register', { 'result': '註冊成功 , 點此跳轉至登入頁面' });
             }
         })
-        
+
     }
 }
 module.exports = registerController
