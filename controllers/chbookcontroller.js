c1f277cda887fa8fb3789432d19c88b81430687d
auror <D0947881@o365.fcu.edu.tw>
2022-05-25 21:11:13 +0800

    chbook

--- controllers/chbookcontroller.js
+++ controllers/chbookcontroller.js
@@ -1,8 +1,31 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'book',
+    password: 'book',
+    database: 'book'
+})
 
 
 const chbookController = {
+    
     chbookPage: (req, res) => {
-        return res.render('chbook')
+        return res.render('chbook', { 'result': '' })
+    },
+    chbookAdd: (req, res) => {
+        var data = req.body;
+        var sql = "INSERT INTO users VALUES('" + "0" + "','" + data.ch_name + "','" + data.address + "','" + data.bookcondition + " ',' " + data.bookprice + "')";
+        console.log(sql)
+        connection.query(sql, function (err, result) {
+            if (err) {
+                return res.render('register', { 'result': 'fail' });
+            }
+            else {
+                return res.render('register', { 'result': 'success' });
+            }
+        })
+
     }
 }
 
