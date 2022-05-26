6124c745f5e4a2f770add3d39097b9f4ebe98389
Yicheng <h0928588211@gmail.com>
2022-05-26 10:07:49 +0800

    cartrepeat

--- controllers/cartcontroller.js
+++ controllers/cartcontroller.js
@@ -6,53 +6,70 @@ const connection = mysql.createConnection({
     password: 'book',
     database: 'book'
 })
-
+var totalprice = 0;
 const cartController = {
     cartPage: (req, res) => {
         if (req.session.userName) {
-            
-            connection.query("SELECT * FROM cart where email ='"+req.session.userName+"'", function (err, result, fields) {
+
+            connection.query("SELECT * FROM cart where email ='" + req.session.userName + "'", function (err, result, fields) {
                 if (err) {
                     throw err;
                 }
                 else {
-                    var totalprice=0;
-                    for(var i=0;i<result.length;i++){
-                        totalprice+=result[i].book_price
+                    totalprice=0;
+                    for (var i = 0; i < result.length; i++) {
+                        totalprice += result[i].book_price
                     }
-                    res.render('cart', {
-                        'cartbook': result, 
-                        'result' : result,
-                        'total': totalprice
+                    res.render('cart', { //渲染頁面，(配合ejs的格式)
+                        'cartbook': result,
+                        'result': result,
+                        'total': totalprice,
+                        'cartstatus': ''
                     });
                 }
             })
         }
         else {
-            res.redirect('/login');
+            res.redirect('/login'); //導向login頁面
         }
 
     },
     addCart: (req, res) => {
         if (req.session.userName) {
-            
-            console.log(typeof(req.params.id));
-            
-            connection.query("SELECT * FROM product where book_id='"+req.params.id+"'",function(err,result){
-                if (err){
-                    throw err;
+
+            // console.log(typeof(req.params.id));
+
+            connection.query("SELECT * FROM product where book_id='" + req.params.id + "'", function (err, result) {
+                if (err) {
+                    return res.render('cart', { 'cartstatus': '已加入購物車' });
                 }
-                else{
-                    console.log(result)
+                else {
+                    // console.log(result)
                     // console.log(result[0].book_id)
-                    var sql = "INSERT INTO cart(email,book_price,book_author,book_id,number) VALUES('"+req.session.userName+"',"+result[0].book_price+",'"+result[0].book_author+"','"+result[0].book_id+"',1)";
-                    console.log(sql);
+                    var sql = "INSERT INTO cart(email,book_price,book_author,book_id,number) VALUES('" + req.session.userName + "'," + result[0].book_price + ",'" + result[0].book_author + "','" + result[0].book_id + "',1)";
+                    // console.log(sql);
                     connection.query(sql, function (err, result) {
                         if (err) {
-                            throw err;
+                            connection.query("SELECT * FROM cart where email ='" + req.session.userName + "'", function (err, result, fields) {
+                                if (err) {
+                                    throw err;
+                                }
+                                else {
+                                    totalprice=0;
+                                    for (var i = 0; i < result.length; i++) {
+                                        totalprice += result[i].book_price
+                                    }
+                                    res.render('cart', { //渲染頁面，(配合ejs的格式)
+                                        'cartbook': result,
+                                        'result': result,
+                                        'total': totalprice,
+                                        'cartstatus': '此書已存在在購物車'
+                                    });
+                                }
+                            })
                         }
                         else {
-                            redirect('/cart');
+                            res.redirect('/cart');
                         }
                     })
                 }
