78350d661189ad306e23bdbeb6304e7f8ddec3e2
Liuming <a723479.a12@gmail.com>
2022-05-26 21:19:46 +0800

    merchant

--- controllers/merchantcontroller.js
+++ controllers/merchantcontroller.js
@@ -1,9 +1,45 @@
 
 
+
+
 const merchantController = {
     merchantPage: (req, res) => {
         return res.render('merchant')
-    }
+    },
+    rebookPage: (req, res) => {
+        var result = [
+            {
+                'book_img':'1.jpg',
+                'username':'liu',
+                'chbookname':'book1',
+                'address':'1408'
+            }
+        ]
+        return res.render('merchantrebook', {'result':result})
+    },
+    chbookPage: (req, res) => {
+        var result = [
+            {
+                'book_img':'1.jpg',
+                'username':'liu',
+                'chbookname':'book1',
+                'address':'1408'
+            }
+        ]
+        return res.render('merchantchbook',{'result':result});
+    },
+    orderPage: (req, res) => {
+        // var result = [
+        //     {
+        //         'buyer':'liu1',
+        //         '':''
+        //     }
+        // ]
+        return res.render('merchantorder');
+    },
+    uploadPage: (req, res) => {
+        return res.render('merchantupload')
+    },
 }
 
 module.exports = merchantController
