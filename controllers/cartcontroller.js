c4c062a8b1aa7433de600fa2ce84a4cab2f35247
Liuming <a723479.a12@gmail.com>
2022-05-26 21:44:28 +0800

    cartmodify

--- controllers/cartcontroller.js
+++ controllers/cartcontroller.js
@@ -16,12 +16,12 @@ const cartController = {
                     throw err;
                 }
                 else {
-                    totalprice=0;
+                    totalprice = 0;
                     for (var i = 0; i < result.length; i++) {
                         totalprice += result[i].book_price
                     }
+                    console.log(result);
                     res.render('cart', { //渲染頁面，(配合ejs的格式)
-                        'cartbook': result,
                         'result': result,
                         'total': totalprice,
                         'cartstatus': ''
@@ -41,34 +41,36 @@ const cartController = {
 
             connection.query("SELECT * FROM product where book_id='" + req.params.id + "'", function (err, result) {
                 if (err) {
-                    return res.render('collect', { 'cartstatus': '已加入購物車' });
+                    return res.render('cart', { 'cartstatus': '已加入購物車' });
                 }
                 else {
                     // console.log(result)
                     // console.log(result[0].book_id)
-                    var sql = "INSERT INTO collect(email,book_id,book_price,book_author) VALUES('" + req.session.userName + "'," + result[0].book_id + ",'" + result[0].book_price + "','" + result[0].author + "')";
+                    var sql = "INSERT INTO cart(email,book_price,book_author,book_id,number) VALUES('" + req.session.userName + "'," + result[0].book_price + ",'" + result[0].book_author + "','" + result[0].book_id + "',1)";
                     // console.log(sql);
                     connection.query(sql, function (err, result) {
                         if (err) {
-                            connection.query("SELECT * FROM collect where email ='" + req.session.userName + "'", function (err, result, fields) {
+                            connection.query("SELECT * FROM cart where email ='" + req.session.userName + "'", function (err, result, fields) {
                                 if (err) {
                                     throw err;
                                 }
                                 else {
-                                    totalprice=0;
+                                    totalprice = 0;
                                     for (var i = 0; i < result.length; i++) {
                                         totalprice += result[i].book_price
                                     }
-                                    res.render('collect', { //渲染頁面，(配合ejs的格式)
+                                    res.render('cart', { //渲染頁面，(配合ejs的格式)
                                         'cartbook': result,
                                         'result': result,
-                                        'cartstatus': '此書已存在在收藏'
+                                        'total': totalprice,
+                                        'cartstatus': '此書已存在在購物車'
                                     });
                                 }
                             })
                         }
                         else {
                             console.log('add to cart success');
+                            res.redirect('/cart');
                         }
                     })
                 }
@@ -99,7 +101,7 @@ const cartController = {
                                     throw err;
                                 }
                                 else {
-                                    totalprice=0;
+                                    totalprice = 0;
                                     for (var i = 0; i < result.length; i++) {
                                         totalprice += result[i].book_price
                                     }
@@ -135,7 +137,7 @@ const cartController = {
                 else {
                     // console.log(result)
                     // console.log(result[0].book_id)
-                    var sql = "DELETE FROM cart Where email='"+ req.session.userName + "' and book_id='" + req.params.id + "'";
+                    var sql = "DELETE FROM cart Where email='" + req.session.userName + "' and book_id='" + req.params.id + "'";
                     // console.log(sql);
                     connection.query(sql, function (err, result) {
                         if (err) {
