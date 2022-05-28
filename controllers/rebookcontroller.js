4734158e459798b502c5961d5058b76d264c4c2a
Liuming <a723479.a12@gmail.com>
2022-05-29 01:19:03 +0800

    rebookInsertSQL

--- controllers/rebookcontroller.js
+++ controllers/rebookcontroller.js
@@ -10,15 +10,49 @@ const connection = mysql.createConnection({
 const rebookController = {
     rebookPage: (req, res) => {
         if (req.session.userName) {
-            connection.query("SELECT * FROM users", function (err, result) {
-                for (var i = 0; i < result.length; i++) {
-                    if (req.session.userName == result[i].email) {
-                        console.log(result)
-                        return res.render('rebook',{'bookcoin':'0'})
-                    }
+            connection.query(`SELECT * FROM users WHERE email = '${req.session.userName}'`, function (err, result) {
+                if (!err){
+                    return res.render('rebook',{'bookcoin': result[0].point})
+                }
+                else    return res.redirect('/login');
+            })
+        }
+        else{
+            return res.redirect('/login');
+        }
+    },
+    rebookAdd : (req, res) => {
+        // console.log(req.body);
+        if (req.session.userName) {
+            var data = req.body;
+            var point = data.re_price;
+            if (data.re_status == '7成~9成'){
+                point *= 0.6;
+                point = point-point%1;
+            }
+            else if (data.re_status == '全新'){
+                point *= 0.5;
+                point = point-point%1;
+            }
+            else if (data.re_status == '5成~7成'){
+                point *= 0.4;
+                point = point-point%1;
+            }
+            connection.query(`SELECT * FROM users WHERE email = '${req.session.userName}'`, function (err, result) {
+                if (!err){
+                    var sql = `INSERT INTO reduce (email,re_name,re_author,re_price,re_status,re_point,re_judge)  VALUES ('${req.session.userName}','${data.re_name}','${data.re_author}','${data.re_price}','${data.re_status}','${point}','0');`;
+                    connection.query(sql, function (err, result1){
+                        if (err){
+                            console.log(err);
+                        }
+                        else{
+                            return res.render('rebook',{'bookcoin': result[0].point})
+                        }
+                    })
                 }
             })
         }
+        else res.redirect('/login');
     }
 }
 
