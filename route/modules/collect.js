1bc526444c8f868c52a35cb5979a16300b5075a1
auror <D0947881@o365.fcu.edu.tw>
2022-05-28 20:04:24 +0800

    collect

--- route/modules/collect.js
+++ route/modules/collect.js
@@ -7,6 +7,7 @@ const collectController = require('../../controllers/collectcontroller')
 
 router.get('/', collectController.collectPage)
 router.get('/add/:id', collectController.addCollect);
+router.get('/del/:id', collectController.delCollect);
 
 
 
