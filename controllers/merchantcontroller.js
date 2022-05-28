15e9c9adcc83884935d4fad7400f0565eb08a72b
Liuming <a723479.a12@gmail.com>
2022-05-28 14:25:56 +0800

    confirmModify

--- controllers/merchantcontroller.js
+++ controllers/merchantcontroller.js
@@ -37,8 +37,9 @@ const merchantController = {
     orderPage: (req, res) => {
         var result = [
             {
-                'order_no':'1',
+                'order_no':'3',
                 'order_name':'liu',
+                'order_email':'liu@gmail.com',
                 'book': [
                     {
                         'book_id':'1',
@@ -54,8 +55,9 @@ const merchantController = {
                 'address':'1408'
             },
             {
-                'order_no':'2',
+                'order_no':'4',
                 'order_name':'liu',
+                'order_email':'liu@gmail.com',
                 'book': [
                     {
                         'book_id':'1',
@@ -78,7 +80,61 @@ const merchantController = {
     },
     confirmOrder: (req, res) => {
         console.log('confirm ',req.params.id);
-        return res.redirect('/merchant/order');
+        //delete user point -> return bonus point -> update order status
+        connection.query(`SELECT * FROM orders where order_id ='${req.params.id}'`, function (err, result, fields) {
+            if (err) {
+                res.redirect('/merchant/order');
+            }
+            else {
+                connection.query(`UPDATE users SET point = point - ${result[0].order_delpoint} WHERE users.email = '${result[0].email}';`, function (err, result){
+                    if (err){
+                        console.log(err);
+                    }
+                    else{
+                        console.log('delpoint success');
+                    }
+                })
+                var sum = 0;
+                connection.query(`SELECT book_price FROM product WHERE book_id = any (SELECT book_id FROM ordersinformation WHERE order_id = ${req.params.id});`, function (err, result){
+                    if (err){
+                        console.log(err);
+                    }
+                    else{
+                        for (let i = 0 ; i < result.length ; i++ ){
+                            connection.query(`SELECT book_price FROM product WHERE book_id = ${result[i].book_id};`, function (err, result){
+                                if (err)    console.log(err);
+                                else{
+                                    console.log(result);
+                                    sum = sum + result[0].book_price;
+                                    console.log(result[0].book_price);
+                                }
+                            })
+                        }
+                        console.log(sum);
+                        return res.redirect('/merchant/order');
+                    }
+                })
+                // connection.query(`UPDATE users SET point = point - ${result[0].delpoint} WHERE email = ${result[0].email}`, function (err, result){
+                //     if (err){
+                //         console.log(err);
+                //     }
+                //     else{
+                //         console.log('addPoint success');
+                //     }
+                // })
+                // console.log(result);
+            }
+        })
+        // connection.query(`UPDATE orders SET order_status = '1' WHERE order_id = '${req.params.id}';`, function (err, result, fields) {
+        //     if (err){
+        //         console.log(err);
+        //     }
+        //     else{
+        //         console.log(`cancel ${req.params.id} success`);
+        //         return res.redirect('/merchant/order');
+        //     }
+        // });
+        
     },
     cancelOrder: (req, res) => {
         // console.log('cancel ', req.params.id);
