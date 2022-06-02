c4900b840fe57276b9cb8f20718735d6bc0f8d16
Jie <qazwsxlip@gmail.com>
2022-06-02 18:22:06 +0800

    Merge branch 'main' of https://github.com/Liuming9124/SYSANA_final

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,14 +1,7 @@
 function compare() {
     var bookcoin = document.getElementById('bookcoin').value;
     var coin = document.getElementById('coin').value;
-    
-    if(bookcoin>coin) {
-        alert('book幣不足')
-        document.getElementById('coin').value='0';
-        return;
-    }
-    else {
-        document.getElementById('buybutton').submit();
-    }
-
+   console.log(coin)
+   var t="<%=delpoint%>";
+   console.log(t)
 }
